0596aabdea931f1a3b0d1d9c3fb529617ea3fe11
sebdal <semoda@bio.aau.dk>
2024-12-13 20:56:19 +0000

    feat: Now uses IupacBase enum | chore: moved redundant code

--- motif/src/lib.rs
+++ motif/src/lib.rs
@@ -1,171 +1,67 @@
-use anyhow::bail;
-use phf::phf_map;
+use anyhow::{bail, Context, Result};
 use regex::Regex;
-use std::fmt;
 
-static IUPAC_MAPPING: phf::Map<&'static str, &'static str> = phf_map! {
-    "R" => "AG",
-    "Y" => "CT",
-    "S" => "CG",
-    "W" => "AT",
-    "K" => "GT",
-    "M" => "AC",
-    "B" => "CGT",
-    "D" => "AGT",
-    "H" => "ACT",
-    "V" => "ACG",
-    "N" => ".",
-};
+mod modtype;
+use modtype::ModType;
 
-static COMPLEMENT_BASE: phf::Map<&'static str, &'static str> = phf_map! {
-    "A" => "T",
-    "T" => "A",
-    "G" => "C",
-    "C" => "G",
-    "R" => "Y",
-    "Y" => "R",
-    "S" => "S",
-    "W" => "W",
-    "K" => "M",
-    "M" => "K",
-    "B" => "V",
-    "D" => "H",
-    "H" => "D",
-    "V" => "B",
-    "." => ".",
-    "[" => "]",
-    "]" => "[",
-    "N" => "N",
-};
-
-// INFO: This is taken from modkit https://github.com/nanoporetech/modkit?tab=License-1-ov-file
-pub enum IupacBase {
-    A,
-    T,
-    G,
-    C,
-    R,
-    Y,
-    S,
-    W,
-    K,
-    M,
-    B,
-    D,
-    H,
-    V,
-    N,
-}
-
-impl IupacBase {
-    pub fn parse_char(base: char) -> anyhow::Result<Self> {
-        match base {
-            'A' => Ok(Self::A),
-            'T' => Ok(Self::T),
-            'G' => Ok(Self::G),
-            'C' => Ok(Self::C),
-            'R' => Ok(Self::R),
-            'Y' => Ok(Self::Y),
-            'S' => Ok(Self::S),
-            'W' => Ok(Self::W),
-            'K' => Ok(Self::K),
-            'M' => Ok(Self::M),
-            'B' => Ok(Self::B),
-            'D' => Ok(Self::D),
-            'H' => Ok(Self::H),
-            'V' => Ok(Self::V),
-            'N' => Ok(Self::N),
-            _ => bail!("Not a defined Iupac base: {base}"),
-        }
-    }
-}
-
-#[derive(Debug, Clone, PartialEq, Eq)]
-pub enum ModType {
-    SixMA,
-    FiveMC,
-    FourMC,
-}
-
-impl ModType {
-    pub fn from_str(mod_type: &str) -> Result<Self, String> {
-        match mod_type {
-            "a" => Ok(ModType::SixMA),
-            "m" => Ok(ModType::FiveMC),
-            "21839" => Ok(ModType::FourMC),
-            _ => Err(format!("Unsupported mod type: {}", mod_type)),
-        }
-    }
-
-    pub fn to_pileup_code(&self) -> &'static str {
-        match self {
-            ModType::SixMA => "a",
-            ModType::FiveMC => "m",
-            ModType::FourMC => "21839",
-        }
-    }
-}
-
-impl fmt::Display for ModType {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        match self {
-            ModType::SixMA => write!(f, "6mA (a)"),
-            ModType::FiveMC => write!(f, "5mC (m)"),
-            ModType::FourMC => write!(f, "4mC (21839)"),
-        }
-    }
-}
+mod iupac;
+use iupac::IupacBase;
 
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub struct Motif {
-    pub sequence: String,
+    pub sequence: Vec<IupacBase>,
     pub mod_type: ModType,
     pub mod_position: u8,
 }
 
 impl Motif {
-    pub fn new(sequence: &str, mod_type: &str, mod_position: u8) -> Result<Self, String> {
+    pub fn new(sequence: &str, mod_type: &str, mod_position: u8) -> Result<Self> {
         let mod_type = ModType::from_str(mod_type)?;
 
-        for b in sequence.chars() {
-            IupacBase::parse_char(b).map_err(|_| {
-                format!(
-                    "Base '{}' in sequence '{}' is not a valid IUPAC code",
-                    b, sequence
-                )
-            })?;
-        }
+        let parsed_sequence = sequence
+            .chars()
+            .map(|b| {
+                IupacBase::parse_char(b).map_err(|_| {
+                    anyhow::anyhow!(
+                        "Base '{}' in sequence '{}' is not a valid IUPAC code",
+                        b,
+                        sequence
+                    )
+                })
+            })
+            .collect::<Result<Vec<IupacBase>>>()?;
 
-        if mod_position as usize > sequence.len() - 1 {
-            return Err(format!(
+        if mod_position as usize > parsed_sequence.len() - 1 {
+            bail!(
                 "mod_position {} is out of bounds for sequence of length {}. Note mod_position is 0-indexed.",
                 mod_position,
-                sequence.len()
-            ));
+                parsed_sequence.len()
+            );
         }
 
-        let base_at_position = sequence.chars().nth(mod_position as usize).unwrap(); // Safe as we have checked bounds above
+        let base_at_position = &parsed_sequence[mod_position as usize];
         match mod_type {
             ModType::SixMA => {
-                if base_at_position != 'A' {
-                    return Err(format!(
+                if *base_at_position != IupacBase::A {
+                    bail!(
                         "mod_position {} points to base '{}' which is invalid for 6mA.",
-                        mod_position, base_at_position
-                    ));
+                        mod_position,
+                        base_at_position
+                    );
                 }
             }
             ModType::FiveMC | ModType::FourMC => {
-                if base_at_position != 'C' {
-                    return Err(format!(
+                if *base_at_position != IupacBase::C {
+                    bail!(
                         "mod_position {} points to base '{}' which is invalid for {} modification type.",
                         mod_position, base_at_position, mod_type
-                    ));
+                    );
                 }
             }
         }
 
         Ok(Self {
-            sequence: sequence.to_string(),
+            sequence: parsed_sequence,
             mod_type,
             mod_position,
         })
@@ -176,12 +72,9 @@ impl Motif {
             // sequence: (&self.sequence.chars().rev().collect::<String>()).to_string(),
             sequence: self
                 .sequence
-                .chars()
+                .iter()
                 .rev()
-                .map(|c| match COMPLEMENT_BASE.get(&c.to_string().as_str()) {
-                    Some(s) => s.to_string(),
-                    None => c.to_string(),
-                })
+                .map(IupacBase::to_complement_base)
                 .collect(),
             mod_type: self.mod_type.clone(),
             mod_position: self.sequence.len() as u8 - self.mod_position - 1,
@@ -189,19 +82,7 @@ impl Motif {
     }
 
     pub fn to_regex(&self) -> String {
-        self.sequence
-            .chars()
-            .map(|c| match IUPAC_MAPPING.get(&c.to_string().as_str()) {
-                Some(s) => {
-                    if *s == "." {
-                        ".".to_string()
-                    } else {
-                        format!("[{}]", s)
-                    }
-                }
-                None => c.to_string(),
-            })
-            .collect()
+        self.sequence.iter().map(IupacBase::to_regex).collect()
     }
 }
 
@@ -221,10 +102,17 @@ pub fn find_motif_indices_in_contig(contig: &str, motif: &Motif) -> Vec<u32> {
 mod tests {
     use super::*;
 
+    fn parse_iupac_sequence(sequence: &str) -> Vec<IupacBase> {
+        sequence
+            .chars()
+            .map(|c| IupacBase::parse_char(c).unwrap())
+            .collect()
+    }
+
     #[test]
     fn test_motif_creation() {
         let motif = Motif::new("GATC", "a", 1).unwrap();
-        assert_eq!(motif.sequence, "GATC");
+        assert_eq!(motif.sequence, parse_iupac_sequence("GATC"));
         assert_eq!(motif.mod_type, ModType::SixMA);
         assert_eq!(motif.mod_position, 1);
     }
@@ -234,7 +122,7 @@ mod tests {
         let result = Motif::new("GATC", "m", 4);
         assert!(result.is_err());
         assert_eq!(
-            result.unwrap_err(),
+            result.unwrap_err().to_string(),
             "mod_position 4 is out of bounds for sequence of length 4. Note mod_position is 0-indexed."
         );
     }
@@ -243,7 +131,7 @@ mod tests {
     fn test_unidentified_motif_type() {
         let result = Motif::new("GATC", "d", 1);
         assert!(result.is_err());
-        assert_eq!(result.unwrap_err(), "Unsupported mod type: d");
+        assert_eq!(result.unwrap_err().to_string(), "Unsupported mod type: d");
     }
 
     #[test]
@@ -251,7 +139,7 @@ mod tests {
         let result = Motif::new("ATCG", "m", 3); // 'G' is invalid for 5mC
         assert!(result.is_err());
         assert_eq!(
-            result.unwrap_err(),
+            result.unwrap_err().to_string(),
             "mod_position 3 points to base 'G' which is invalid for 5mC (m) modification type."
         );
     }
@@ -261,7 +149,7 @@ mod tests {
         let result = Motif::new("ATZG", "a", 0); // 'G' is invalid for 5mC
         assert!(result.is_err());
         assert_eq!(
-            result.unwrap_err(),
+            result.unwrap_err().to_string(),
             "Base 'Z' in sequence 'ATZG' is not a valid IUPAC code"
         );
     }
@@ -271,9 +159,18 @@ mod tests {
         let motif1 = Motif::new("GATC", "m", 3).unwrap();
         let motif2 = Motif::new("TCCCG", "m", 1).unwrap();
         let motif3 = Motif::new("RGATCY", "a", 2).unwrap();
-        assert_eq!(motif1.reverse_complement().sequence, "GATC");
-        assert_eq!(motif2.reverse_complement().sequence, "CGGGA");
-        assert_eq!(motif3.reverse_complement().sequence, "RGATCY");
+        assert_eq!(
+            motif1.reverse_complement().sequence,
+            parse_iupac_sequence("GATC")
+        );
+        assert_eq!(
+            motif2.reverse_complement().sequence,
+            parse_iupac_sequence("CGGGA")
+        );
+        assert_eq!(
+            motif3.reverse_complement().sequence,
+            parse_iupac_sequence("RGATCY")
+        );
         assert_eq!(
             motif1.reverse_complement().mod_type,
             ModType::from_str("m").unwrap()
