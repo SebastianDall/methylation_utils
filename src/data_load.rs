a2e50289fef255a8f4c75e41f632009024535827
SebastianDall <semoda@bio.aau.dk>
2024-12-19 14:58:39 +0100

    Changed type to hashmap

--- src/data_load.rs
+++ src/data_load.rs
@@ -1,14 +1,15 @@
-use anyhow::{Context, Result};
+use ahash::AHashMap;
+use anyhow::Context;
 use seq_io::fasta::{Reader, Record};
 use std::path::Path;
 
-use crate::data::{contig::Contig, GenomeWorkspace};
+use crate::data::contig::Contig;
 
-pub fn load_contigs<P: AsRef<Path>>(path: P) -> Result<GenomeWorkspace> {
+pub fn load_contigs<P: AsRef<Path>>(path: P) -> anyhow::Result<AHashMap<String, Contig>> {
     let mut fasta_reader = Reader::from_path(&path)
         .with_context(|| format!("Failed to open FASTA at: {:?}", path.as_ref()))?;
 
-    let mut contigs = GenomeWorkspace::new();
+    let mut contigs = AHashMap::new();
 
     while let Some(record_result) = fasta_reader.next() {
         let record = record_result.with_context(|| "Error reading record from FASTA file.")?;
@@ -16,13 +17,13 @@ pub fn load_contigs<P: AsRef<Path>>(path: P) -> Result<GenomeWorkspace> {
         let id = record
             .id()
             .map(String::from)
-            .with_context(|| "Error extracing record ID")?;
+            .with_context(|| "Error extracting record ID")?;
 
         let seq = String::from_utf8(record.owned_seq())
             .with_context(|| format!("Invalid UTF8 character in FASTA record: '{}'", id))?
             .to_string();
 
-        contigs.add_contig(Contig::new(id, seq))?;
+        contigs.insert(id.clone(), Contig::new(id, seq));
     }
     Ok(contigs)
 }
